5a9f76432c199a9dec6477a82d7971685bc13c8e
yatharth-gupta <guptayatharth1@gmail.com>
2023-05-08 00:58:30 +0530

    new-changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
 
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
