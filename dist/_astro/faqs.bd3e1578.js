64d6bf661d176bf6d1ee4a3f2ce6b7e379faea5d
yatharth gupta <guptayatharth1@gmail.com>
2023-06-27 16:08:36 +0530

    fixed-home-page-images

--- dist/_astro/faqs.bd3e1578.js
+++
@@ -1,1 +0,0 @@
-import{default as a}from"./faqs.c2ff025c.js";import"./index.87b0c0b1.js";import"./index.50d91d22.js";import"./load.874eb0c9.js";import"./index.3e43d6d8.js";import"./loader.9b014291.js";import"./jsx-runtime.37f0d4ec.js";export{a as default};
---
+++ src/components/testimonials.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import Loader from "./loader";
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+export default function Testimonials(){
+
+    const [sheetdata, setSheetdata] = useState({});
+    const [loaded, setLoaded] = useState(false);
+    
+    useEffect(()=>{
+        const fetch_func = async () => {
+            const url = "https://script.google.com/macros/s/AKfycbzapvDQRF3bY8dm5MYFVWqFjg83NibzZy0Dsid9VnucIxwCohPRxGMVTImgGoYjvrs/exec?q=testimonials";
+            const response = await fetch(url);
+            const info = await response.json();
+            const data = info.data.data;
+            console.log(data);
+            setSheetdata(data);
+            setLoaded(true);
+        }
+        fetch_func();
+    }, []);
+
+    return(
+        <>
+        {loaded ? (
+            <>
+        {Object.values(sheetdata).map((c,i)=>{
+            console.log(c[2]);
+            return(
+                <div style={{marginBottom: "3rem"}}>
+                    <span class="icon">
+                        <FontAwesomeIcon icon={faQuoteLeft} />
+                    </span>
+                    <span style={{paddingLeft: "2rem", paddingRight: "2rem", lineHeight:"2"}}>{c[2]}</span>
+                    <span class="icon">
+                        <FontAwesomeIcon icon={faQuoteRight} />
+                    </span>
+                    <p style={{textAlign: "right", width: "100%"}}>{"- " + c[0] + " " + c[1]}</p>
+                </div>
+            );
+        })}
+        </>
+        ):(<Loader/>)}
+        </>
+    );
+}
