c347cf38260ef5fad6addd93cc3c37510c33ca8a
112Nisha <nishita.kannan@students.iiit.ac.in>
2024-06-24 09:27:44 +0530

    Random testimonial loading from google scripts

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -5,54 +5,35 @@ import { faQuoteLeft, faQuoteRight } from "@fortawesome/free-solid-svg-icons";
 import Loader from "./loader";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const id = "1SUOs97mV0MUgad0shGq2Kfpl24RHyvSjtbzR4VrrRjM";
-const gid = "1581174192";
-const url = `https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:json&tq&gid=${gid}`;
+const url = "https://script.google.com/macros/s/AKfycbzvYgAwebXfLnmcN0PJmK9AqApj_eeCOqL_J-s25nCkhV-5QKXrxOTUMjHJCSnJTSlu/exec";
 
 export default function Testimonials() {
   const [sheetdata, setSheetdata] = useState([]);
   const [loaded, setLoaded] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        const jsonData = await response.text();
+    const FetchData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
 
-        const startIndex = jsonData.indexOf('{"version":"0.6"');
-        const endIndex = jsonData.lastIndexOf('}') + 1;
-        const jsonString = jsonData.substring(startIndex, endIndex);
-        const parsedData = JSON.parse(jsonString);
+      const extractedTestimonials = data.map((row) => {
+        const [associationType, year, testimonial] = row;
+        return { associationType, year, testimonial };
+      });
 
-        const extractedTestimonials = parsedData.table.rows.map((row) => {
-          const [associationType, , testimonial] = row.c;
-          return { associationType: associationType.v, testimonial: testimonial.v };
-        });
-
-        setSheetdata(extractedTestimonials);
-        setLoaded(true);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+      setSheetdata(extractedTestimonials);
+      setLoaded(true);
     };
-    fetchData();
+    FetchData();
   }, []);
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 4) % sheetdata.length);
-  };
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 4 + sheetdata.length) % sheetdata.length);
-  };
 
   return (
     <>
       {loaded ? (
         <div className="testimonial-container">
           <div className="testimonial-scroll">
-            {sheetdata.slice(currentIndex, currentIndex + 4).map((data, i) => (
+            {sheetdata.map((data, i) => (
               <div key={i} className="testimonial-card">
                 <span className="icon">
                   <FontAwesomeIcon icon={faQuoteLeft} />
@@ -61,13 +42,9 @@ export default function Testimonials() {
                 <span className="icon">
                   <FontAwesomeIcon icon={faQuoteRight} />
                 </span>
-                <p className="testimonial-author">- {data.associationType}</p>
+                <p className="testimonial-author">- {data.associationType}, {data.year}</p>
               </div>
             ))}
-          </div>
-          <div className="testimonial-navigation">
-            <button onClick={handlePrev}>Previous</button>
-            <button onClick={handleNext}>Next</button>
           </div>
         </div>
       ) : (
